88f44a95c61abcf207bd252e597d12f4aeee08e3
Gustavo Sanchez <3004395470101@ingenieria.usac.edu.gt>
2022-02-25 00:21:47 -0600

    [ADD] Login Page
    Se agrego la pagina de login

---
+++ faunadex-app/src/components/nav/NavbarLogin.tsx
@@ -0,0 +1,52 @@
+import { NavLink } from 'react-router-dom';
+export const NavbarLogin = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark shadow-5-strong">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/login">
+            <i className="fa-solid fa-paw me-2"></i>
+            Faunadex
+          </NavLink>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive ? 'nav-link active' : 'nav-link'
+                  }
+                  to="/login"
+                >
+                  Login
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive ? 'nav-link active' : 'nav-link'
+                  }
+                  to="/register"
+                >
+                  Registro
+                </NavLink>
+              </li>
+            </ul>
+            <ul className="navbar-nav d-flex">
+              <li className="nav-item ">
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive ? 'nav-link active' : 'nav-link'
+                  }
+                  to="/register"
+                >
+                  <i className="fa-brands fa-github me-2"></i>
+                  GITHUB
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
---
+++ faunadex-app/src/routes/PrivateRoutes.tsx
@@ -0,0 +1,3 @@
+export const PrivateRoutes = () => {
+  return <div>PrivateRoutes</div>;
+};
