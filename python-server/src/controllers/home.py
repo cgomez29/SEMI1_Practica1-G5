c739cb6c6b9306e3e9eb6a6ce800b302abcc26f5
Kevin Sandoval <2819606230104@ingenieria.usac.edut.gt>
2022-03-02 20:37:45 -0600

    Added /api/test route

--- python-server/src/controllers/home.py
+++ python-server/src/controllers/home.py
@@ -4,6 +4,14 @@ from flask import jsonify
 @app.route('/', methods=['GET'])
 def home():
     try:       
+        return jsonify({'message': "Home from python", 'status': True})
+        
+    except Exception:
+        return jsonify({'message': "Error server from python", 'status': False})
+
+@app.route('/api/test', methods=['GET'])
+def test():
+    try:
         return jsonify({'message': "Server from python is working", 'status': True})
         
     except Exception:
